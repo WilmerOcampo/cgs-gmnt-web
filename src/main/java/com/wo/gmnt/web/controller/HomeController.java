dd1d184fdd44f8c9238422805ef945ed82db60eb
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-09-26 00:21:54 -0500

    Refactoring gui

--- src/main/java/com/wo/gmnt/web/controller/HomeController.java
+++ src/main/java/com/wo/gmnt/web/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com.wo.gmnt.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
 public class HomeController {
-    @RequestMapping("")
+
+    @GetMapping("")
     public String home() {
         return "home";
     }
+
+    @GetMapping("/modals")
+    public String modals() {
+        return "modals";
+    }
+
 }

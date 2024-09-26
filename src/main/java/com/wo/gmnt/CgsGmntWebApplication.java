3eaa3a2544f078fbf15309b32bd7fada388bb969
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-09-25 19:21:48 -0500

    Uploading project

---
+++ src/main/java/com/wo/gmnt/CgsGmntWebApplication.java
@@ -0,0 +1,13 @@
+package com.wo.gmnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgsGmntWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgsGmntWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wo/gmnt/CgsGmntWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wo.gmnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgsGmntWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
